52c1107616b48f94ebd497e9756a5021a598ba27
Aktan Azat <aazat@ucdavis.edu>
2025-12-02 13:45:10 -0800

    created moderationservicecontroler

---
+++ spring-boot-app/src/main/java/com/ecs160/hw2/ModerationServiceController.java
@@ -0,0 +1,14 @@
+package com.ecs160.hw2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ModerationServiceController {
+
+    @GetMapping("/find_bugs")
+    public String findBugs(@RequestParam String code) {
+        return "[]";
+    }
+}
