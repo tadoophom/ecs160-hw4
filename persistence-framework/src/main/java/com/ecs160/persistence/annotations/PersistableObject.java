b66b49107747fd9c91f099a836ec9732ffb9e9ee
Aktan Azat <aazat@ucdavis.edu>
2025-12-12 21:37:18 -0800

    restored persistence framework

---
+++ persistence-framework/src/main/java/com/ecs160/persistence/annotations/PersistableObject.java
@@ -0,0 +1,11 @@
+package com.ecs160.persistence.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PersistableObject {
+}
